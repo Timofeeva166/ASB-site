be901c82f4791b91de6fcb7dd01dc40005ee7e53
Julia Timofeeva <J-Timofeeva166@yandex.ru>
2026-04-24 21:43:15 +0300

    New menu on main page

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,36 @@
 /** СКРИПТЫ ДЛЯ ГЛАВНОЙ СТРАНИЦЫ **/
-
+import { onClick, iconElse } from "./popover.js";
 import { loadTheme, toggleTheme, themeContainer } from "./theme.js";
+
+const indexArr = [
+  {
+    iconName: 'rules',
+    iconText: 'правила',
+    href: 'rules.html'
+  },
+  {
+    iconName: 'template',
+    iconText: 'шаблон резюме',
+    href: './files/resume.docx'
+  },
+  {
+    iconName: 'staff',
+    iconText: 'штат',
+    href: 'staff.html'
+  },
+  {
+    iconName: 'partners',
+    iconText: 'партнерство',
+    href: 'partners.html'
+  },
+  {
+    iconName: 'benefits',
+    iconText: 'премиум-услуги',
+    href: 'benefits.html'
+  }
+];
+
+iconElse?.addEventListener('click', (e) => onClick(e, indexArr));
 
 const cards = document.querySelectorAll('.staff__card-list-item');
 const closeButtons = document.querySelectorAll('.close-icon');
